28e4837a4ffc2c44d1ba5a0abb7f4902596dc718
Jinri Yoo <zzznly@ogqcorp.com>
2024-08-17 01:38:02 +0900

    테스트 코드 작성 - className에 따른 css class 설정

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,9 +1,11 @@
-import cn from 'classnames';
-import React, { useState } from 'react';
 import './text.css';
 
+import React, { useState } from 'react';
+
+import cn from 'classnames';
+
 /**
- * placeholder 설정 - 테스트 작성 완료
+ * placeholder 설정
  * className에 따른 css class 설정 - 테스트 작성 완료
  * 텍스트를 입력할 때마다 onChange 핸들러 호출
  * focus 시 border 스타일 변경
--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -1,9 +1,29 @@
-import { screen } from '@testing-library/react';
+/* eslint-disable import/order */
 import React from 'react';
-
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
+import { screen } from '@testing-library/react';
 
-it('sample test.', async () => {
-  expect(1).toBe(1);
+it('className prop으로 설정한 css class가 적용된다.', async () => {
+  // ** Arrange - 테스트를 위한 환경 만들기
+  // => className을 지닌 컴포넌트 렌더링
+
+  // render API를 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링된 DOM구조가 반영
+  // jsDOM: Node.js에서 사용하기 위해 많은 웹표준을 순수 자바스크립트로 구현
+  await render(<TextField className="my-class" />);
+
+  // ** Act - 테스트할 동작 발생
+  // => 렌더링에 대한 검증이므로 생략
+  // => 클릭이나 메서드 호출, prop 변경 등이 이에 해당
+
+  // ** Assert - 올바른 동작 실행되었는지 검증
+  // => 렌더링 후에 DOM에 해당 class가 존재하는지 검증
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+  screen.debug(); // jsdom에서 DOM 구조 확인
+
+  // className이라는 내부 prop이나 state 값을 검증 (X)
+  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 화면은 DOM 이므로
+  expect(textInput).toHaveClass('my-class');
 });
