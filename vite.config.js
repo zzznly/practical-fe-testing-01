a9d7d256a20ac654866d11fb193b942d71854211
Jinri Yoo <zzznly@ogqcorp.com>
2024-08-17 16:55:16 +0900

    chore

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import path from 'path';
-
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 import { defineConfig } from 'vitest/config';
+import eslint from 'vite-plugin-eslint';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
