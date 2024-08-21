4892e5af71d548f61545730a5797456eb41d062d
Jinri Yoo <zzznly@gmail.com>
2024-08-21 23:38:12 +0900

    ErrorPage, NotFountPage 단위테스트 작성 (모킹 포함)

--- src/pages/error/components/tests/NotFoundPage.spec.jsx
+++ src/pages/error/components/tests/NotFoundPage.spec.jsx
@@ -1,7 +1,20 @@
-import { screen } from '@testing-library/react';
-import React from 'react';
-
 import NotFoundPage from '@/pages/error/components/NotFoundPage';
+import React from 'react';
 import render from '@/utils/test/render';
+import { screen } from '@testing-library/react';
 
-it('Home으로 이동 버튼 클릭시 홈 경로로 이동하는 navigate가 실행된다', async () => {});
+const navigateFn = vi.fn(); // spy 함수
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  return { ...original, useNavigate: () => navigateFn };
+});
+
+it('Home으로 이동 버튼 클릭시 홈 경로로 이동하는 navigate가 실행된다', async () => {
+  const { user } = render(<NotFoundPage />);
+
+  await user.click(screen.getByText('Home으로 이동'));
+
+  expect(navigateFn).toHaveBeenNthCalledWith(1, '/', { replace: true });
+});
